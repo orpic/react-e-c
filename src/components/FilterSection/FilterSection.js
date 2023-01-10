35f4bb53ff831612d27e316f3d33e58d8d1722b3
shobhit-u <shobhitumrao5@gmail.com>
2023-01-11 02:57:39 +0530

    filter are complete

--- src/components/FilterSection/FilterSection.js
+++ src/components/FilterSection/FilterSection.js
@@ -3,9 +3,51 @@ import React from "react";
 import classes from "./FilterSection.module.css";
 
 const FilterSection = () => {
+  const filterList = [
+    {
+      title: "color",
+      tags: ["red", "blue", "green"],
+    },
+    {
+      title: "gender",
+      tags: ["men", "women"],
+    },
+    {
+      title: "price",
+      tags: [" < ₹250", "₹251 - ₹450", " > ₹451"],
+    },
+    {
+      title: "type",
+      tags: ["polo", "hoodie", "basic"],
+    },
+  ];
   return (
     <>
-      <div className={classes.filterArea}></div>
+      <div className={classes.filterArea}>
+        <h2>Filters</h2>
+        {filterList.map((eachFilter) => (
+          <>
+            <p className={classes.filterSubHeading}>{eachFilter.title}</p>
+            <div className={classes.leftMargin}>
+              {eachFilter.tags.map((eachTag) => (
+                <label className={classes.container}>
+                  {eachTag}
+                  <input type="checkbox" />
+                  <span className={classes.checkmark}></span>
+                </label>
+              ))}
+            </div>
+          </>
+        ))}
+        <p className={classes.filterSubHeading}>H</p>
+        <div className={classes.leftMargin}>
+          <label className={classes.container}>
+            One
+            <input type="checkbox" />
+            <span className={classes.checkmark}></span>
+          </label>
+        </div>
+      </div>
     </>
   );
 };
