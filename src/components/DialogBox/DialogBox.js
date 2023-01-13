f0517f8defb927ef7c31d80d63a29db46954976c
shobhit-u <shobhitumrao5@gmail.com>
2023-01-13 17:26:32 +0530

    error modal

---
+++ src/components/DialogBox/DialogBox.js
@@ -0,0 +1,21 @@
+import React from "react";
+//eslint-disbale-next-line
+import classes from "./DialogBox.module.css";
+const DialogBox = ({ isOpen, setIsOpen, dialogMessage }) => {
+  const closeDialogHandler = () => {
+    setIsOpen(false);
+  };
+  return (
+    <div className={`${classes.dialogBox} ${isOpen ? classes.open : ""}`}>
+      <div className={classes.dialogContent}>
+        <p className={classes.title}>{dialogMessage.title}</p>
+        <p className={classes.message}>{dialogMessage.message}</p>
+        <div onClick={closeDialogHandler} className={classes.btn}>
+          OK
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default DialogBox;
--- src/components/ProductCard/ProductCard.js
+++ src/components/ProductCard/ProductCard.js
@@ -1,6 +1,7 @@
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { cartActions } from "../../store/cartSlice";
+import { DialogBox } from "../../components";
 // eslint-disable-next-line
 import classes from "./ProductCard.module.css";
 const ProductCard = ({
@@ -11,19 +12,68 @@ const ProductCard = ({
   productPrice,
 }) => {
   const dispatch = useDispatch();
+  const [isOpen, setIsOpen] = useState(false);
+  const [dialogMessage, setDialogMessage] = useState({
+    title: "",
+    message: "",
+  });
+  const [quantityLeft, setQuantityLeft] = useState(0);
+  const [cartQuantity, setCartQuantity] = useState(0);
+  // const products = useSelector((state) => state.product.products);
+  const cart = useSelector((state) => state.cart.items);
+
+  useEffect(() => {
+    // const product = products.find((item) => item.id === id);
+
+    const cartItem = cart.find((item) => item.id === id);
+    console.log(cartItem);
+    if (cartItem) {
+      setQuantityLeft(quantity - cartItem.quantity);
+      setCartQuantity(cartItem.quantity);
+    } else {
+      setQuantityLeft(quantity);
+      setCartQuantity(0);
+    }
+  }, [quantity, cart, id]);
+
   const addToCartHandler = () => {
-    dispatch(
-      cartActions.addItemToCart({
-        id: id,
-        imageUrl: productImage,
-        name: productName,
-        price: productPrice,
-      })
-    );
+    console.log("clicked");
+    console.log(quantityLeft, cartQuantity);
+    if (quantityLeft > 0 && cartQuantity === 0) {
+      console.log(" > 0 , === 0 ");
+      dispatch(
+        cartActions.addItemToCart({
+          id: id,
+          imageUrl: productImage,
+          name: productName,
+          price: productPrice,
+        })
+      );
+    }
+    if (quantityLeft > 0 && cartQuantity > 0) {
+      console.log(" > 0 , > 0 ");
+      //dialog box
+      setIsOpen(true);
+      setDialogMessage({ title: "Want more?", message: "Add on Cart page" });
+    }
+    if (quantityLeft === 0) {
+      console.log(" === 0 ,  -- ");
+      //dialog box
+      setIsOpen(true);
+      setDialogMessage({
+        title: "Stock out",
+        message: "We will re-stock soon",
+      });
+    }
   };
   return (
     <>
       <div className={classes.cardContainer}>
+        <DialogBox
+          isOpen={isOpen}
+          setIsOpen={setIsOpen}
+          dialogMessage={dialogMessage}
+        />
         <img
           className={classes.productImage}
           src={productImage}
