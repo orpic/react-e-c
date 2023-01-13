4c5ba5e0918b91852313d8562bc5aeadd7c5a028
shobhit-u <shobhitumrao5@gmail.com>
2023-01-13 21:13:57 +0530

    fix

--- src/App.js
+++ src/App.js
@@ -44,8 +44,6 @@ function App() {
     }
   }, [status, products, error, dispatch]);
 
-  ////////////////////////////////////////////////////////////////////////
-
   return (
     <>
       <Layout>
